e5ae74f15613e6bf3eeccdaa76fdd6e8cfd9fc57
ifked <ifkedorland15@gmail.com>
2020-02-03 21:54:56 +0200

    Import function 1
    Defined function 1 in .py file and imported it to the Project 2 Notebook.
    
    Have also tested the function in the Project 2 Notebook

--- eskom_helper.py
+++ eskom_helper.py
@@ -1,2 +1,16 @@
-def hello():
-    print('hello world')
+import numpy as np
+import pandas as pd
+
+def dictionary_of_metrics(items):
+
+    arr = np.array(items)
+
+    #create a dictionary
+    my_dict = {'mean': round(arr.mean(),2),
+                'median': round(np.median(items),2),
+                'variance': round(np.var(items),2),
+                'standard deviation': round(np.std(items),2),
+                'min': round(arr.min(),2),
+                'max': round(arr.max(),2)}
+    
+    return my_dict
